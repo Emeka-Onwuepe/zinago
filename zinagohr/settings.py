d4372a6bb0e2c975ac0d53272a705b90f82d033a
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2022-08-26 22:13:02 +0100

    feat: added allowed host

--- zinagohr/settings.py
+++ zinagohr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY_8")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*zinagohrsolutions.ng"]
 
 
 # Application definition
