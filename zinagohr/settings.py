637e4e21509a0128864bc44191975f640c907975
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2022-08-26 18:08:04 +0100

    fix: changed secret key

--- zinagohr/settings.py
+++ zinagohr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m!@wyy78jsu(%a_(*v_li2wlud@o5+)0)0n-xlb^*=1n&di2md'
+SECRET_KEY = os.getenv("SECRET_KEY_8")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
